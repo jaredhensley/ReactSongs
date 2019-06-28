deba4f1eb7fe2ad99805b41ab795ff281afa0ff3
Jared Hensley <jahensle85@me.com>
2019-06-28 18:50:41 -0500

    selectedsong reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        { title: 'Hey Jude', duration: '4:05' },
+        { title: 'All Star', duration: '5:11' },
+        { title: 'Miami', duration: '3:57' },
+        { title: 'I Want it That Way', duration: '1:45' }
+    ]
+}
